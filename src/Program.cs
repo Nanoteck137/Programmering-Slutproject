3cf9c001e0ebcdc88c3ac691c6253f9b02ddb912
Nanoteck137 <patrik.millvik@gmail.com>
2020-04-23 14:38:21 +0200

    Got a window and cleared it to red

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
 
+using SFML.Window;
+using SFML.Graphics;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Best Pong", Styles.Default);
+
+        window.Closed += Window_Closed;
+
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+
+            window.Clear(Color.Red);
+            window.Display();
+        }
+
+        window.Dispose();
+    }
+
+    private static void Window_Closed(object sender, EventArgs e)
+    {
+        RenderWindow window = (RenderWindow)sender;
+        window.Close();
     }
 }
