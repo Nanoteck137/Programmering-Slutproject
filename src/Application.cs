e9b552c65328c47c651282808c6be71894b01727
Nanoteck137 <patrik.millvik@gmail.com>
2020-04-23 14:41:37 +0200

    Created the application class

---
+++ src/Application.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using SFML.Window;
+using SFML.Graphics;
+
+public class Application
+{
+    private RenderWindow window;
+
+    public Application()
+    {
+        this.window = new RenderWindow(new VideoMode(800, 600), "Best Pong", Styles.Default);
+        window.Closed += Window_Closed;
+    }
+
+    public void Run()
+    {
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+
+            window.Clear(Color.Red);
+            window.Display();
+        }
+
+        window.Dispose();
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+        RenderWindow window = (RenderWindow)sender;
+        window.Close();
+    }
+
+    static void Main(string[] args)
+    {
+        Application app = new Application();
+        app.Run();
+    }
+}
